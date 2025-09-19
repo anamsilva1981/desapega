b40b6da5350300584a2abfc24f7a96b44d4e0703
anamsilva1981 <ana.msilva@hotmail.com>
2025-09-18 22:00:10 -0300

    feat(config): adiciona HttpWrapperService

---
+++ src/app/core/services/http-wrapper.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from './../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpWrapperService {
+
+  private readonly apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  private getHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+  }
+
+  get<T>(endpoint: string): Observable<T> {
+    return this.http.get<T>(`${this.apiUrl}/${endpoint}`, {
+      headers: this.getHeaders(),
+    });
+  }
+
+  post<T>(endpoint: string, body: any): Observable<T> {
+    return this.http.post<T>(`${this.apiUrl}/${endpoint}`, body, {
+      headers: this.getHeaders(),
+    });
+  }
+
+  put<T>(endpoint: string, body: any): Observable<T> {
+    return this.http.put<T>(`${this.apiUrl}/${endpoint}`, body, {
+      headers: this.getHeaders(),
+    });
+  }
+
+  delete<T>(endpoint: string): Observable<T> {
+    return this.http.delete<T>(`${this.apiUrl}/${endpoint}`, {
+      headers: this.getHeaders(),
+    });
+  }
+}
