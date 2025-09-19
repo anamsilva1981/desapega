cacacea357855156b6b5b8a40f420ce36d9df02a
anamsilva1981 <ana.msilva@hotmail.com>
2025-09-18 22:02:43 -0300

    feat(config): ajusta aoo.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
+import { appRoutes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { coreConfig } from './core/core.config';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(appRoutes),
+    provideHttpClient(withInterceptors([])),
+    ...coreConfig.providers
+  ]
 };
