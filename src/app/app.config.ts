35939c28acba2b8c2f7c7a643841f41573a97340
anamsilva1981 <ana.msilva@hotmail.com>
2025-09-25 08:35:36 -0300

    fix(interceptor:) corrige interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,24 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { appRoutes } from './app.routes';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { routes } from './app.routes';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 import { coreConfig } from './core/core.config';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AuthInterceptor } from './core/interceptors/auth.interceptor';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(appRoutes),
-    provideHttpClient(withInterceptors([])),
-    ...coreConfig.providers
+    provideAnimations(),
+    provideRouter(routes),
+    // provideHttpClient(withInterceptors([AuthInterceptor])),
+    ...coreConfig.providers,
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
   ]
 };
