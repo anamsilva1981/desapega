54639e2a5597981debd7ce18935a6368cf08039e
anamsilva1981 <ana.msilva@hotmail.com>
2025-09-25 08:39:58 -0300

    feat(guard):adiciona guarda de rota

---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,16 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (authService.isLoggedIn()) {
+    return true; // permite acesso
+  } else {
+    // redireciona para login se não tiver token
+    router.navigate(['/login']);
+    return false;
+  }
+};
---
+++ src/app/core/guards/public.guard.ts
@@ -0,0 +1,16 @@
+
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+export const publicGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (authService.isLoggedIn()) {
+    // se já estiver logado, manda para dashboard (ou outra rota privada)
+    router.navigate(['/dashboard']);
+    return false;
+  }
+  return true; // permite acessar rota pública (login, register, etc.)
+};
