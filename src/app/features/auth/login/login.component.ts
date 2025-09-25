13eebc0a5edae679a50530b46054d3f9c1a921ee
anamsilva1981 <ana.msilva@hotmail.com>
2025-09-25 08:38:19 -0300

    feat(login): adiciona componente login juntamente com logica e html

---
+++ src/app/features/auth/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from './../../../core/services/auth.service';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  imports: [FormsModule, ReactiveFormsModule, NgIf, RouterModule]
+})
+export class LoginComponent {
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+
+  onSubmit() {
+    if (this.loginForm.invalid) return;
+
+    const { email, password } = this.loginForm.value;
+
+    this.authService.login({ email: email!, password: password! }).subscribe({
+      next: () => this.router.navigate(['/dashboard']),
+      error: (err: any) => console.error('Erro ao logar:', err)
+    });
+  }
+}
